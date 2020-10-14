bdae5722db261ae727b380818cfa929a9d551421
Ngoc Nguyen <30312571+neeooneeoon@users.noreply.github.com>
2020-10-14 23:17:14 +0700

    time update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function makeTimer() {
-    var endTime = new Date("15 October 2020 18:00:00 GMT+07:00");			
+    var endTime = new Date("15 October 2020 20:00:00 GMT+07:00");			
       endTime = (Date.parse(endTime) / 1000);
 
       var now = new Date();
@@ -12,12 +12,12 @@ function makeTimer() {
       var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
       var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
 
-if (days < "10") {days = "0" + days; }
+      if (days < "10") {days = "0" + days; }
       if (hours < "10") { hours = "0" + hours; }
       if (minutes < "10") { minutes = "0" + minutes; }
       if (seconds < "10") { seconds = "0" + seconds; }
 
-$("#time").html(days+":"+hours+":"+minutes+":"+seconds);
+      $("#time").html(days+":"+hours+":"+minutes+":"+seconds);
 
 }
 
